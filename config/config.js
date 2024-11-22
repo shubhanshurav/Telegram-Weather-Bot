2cb46d82baa9d799a403e91200b14b503c768879
shubhanshurav <shubhanshurao7037@gmail.com>
2024-11-22 11:09:02 +0530

    update the code

--- config/config.js
+++ config/config.js
@@ -4,4 +4,5 @@ module.exports = {
   botToken: process.env.BOT_TOKEN,
   weatherApiKey: process.env.WEATHER_API_KEY,
   admins: new Set([1543059186]),
+  botUrl: `https://telegram-bot-for-weather-update.onrender.com/bot${process.env.BOT_TOKEN}`,
 };
--- index.js
+++ index.js
@@ -1,4 +1,18 @@
-const bot = require("./bot/bot");
+const express = require("express");
+const app = express();
+const { botToken, botUrl } = require("./config/config");
+const TelegramBot = require("node-telegram-bot-api");
+const bot = new TelegramBot(botToken);
+const port = process.env.PORT || 3000;
 
-const PORT = process.env.PORT || 3000;
-console.log(`Application running on port ${PORT}`);
+bot.setWebHook(botUrl);
+
+app.use(express.json());
+app.post(`/bot${botToken}`, (req, res) => {
+  bot.processUpdate(req.body);
+  res.sendStatus(200);
+});
+
+app.listen(port, () => {
+  console.log(`Bot running on port ${port}`);
+});
