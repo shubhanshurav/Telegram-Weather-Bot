76576a3c6cbda19b022ebb5ad90f4355013e5037
shubhanshurav <shubhanshurao7037@gmail.com>
2024-11-22 11:47:05 +0530

    update the code

--- index.js
+++ index.js
@@ -1,20 +1,7 @@
 const express = require("express");
 const app = express();
-const { botToken, botUrl } = require("./config/config");
-const TelegramBot = require("node-telegram-bot-api");
-const bot = new TelegramBot(botToken);
+const bot = require("./bot/bot");
 const port = process.env.PORT || 3000;
-const webhookUrl = botUrl;
-console.log(botUrl)
-
-
-bot.setWebHook(webhookUrl);
-
-app.use(express.json());
-app.post(`/bot/${botToken}`, (req, res) => {
-  bot.processUpdate(req.body);
-  res.sendStatus(200);
-});
 
 app.listen(port, () => {
   console.log(`Bot running on port ${port}`);
