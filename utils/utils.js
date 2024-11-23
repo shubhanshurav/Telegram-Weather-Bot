4693b75293e7d2a111eb2545eb4cd145fd00ef18
shubhanshurav <shubhanshurao7037@gmail.com>
2024-11-23 16:16:47 +0530

    final code

--- utils/utils.js
+++ utils/utils.js
@@ -3,6 +3,7 @@ const { WEATHER_API_KEY } = require("../config/config");
 
 const fs = require("fs");
 const path = require("path");
+const bot = require("../bot/bot");
 
 function saveConfig(config) {
   const configPath = path.join(__dirname, "../config/config.json");
@@ -38,6 +39,28 @@ async function validateApiKey(apiKey) {
   }
 }
 
+async function validateBotToken(token) {
+  try {
+    // Send a GET request to the `getMe` method of Telegram Bot API
+    const response = await axios.get(
+      `https://api.telegram.org/bot${token}/getMe`
+    );
+console.log(response.data)
+    if (response.data.ok) {
+      // Bot token is valid
+      console.log("Bot token is valid!");
+      return true;
+    } else {
+      // Invalid token
+      console.log("Invalid bot token.");
+      return false;
+    }
+  } catch (error) {
+    console.error("Error while validating bot token:", error.message);
+    return false;
+  }
+}
+
 async function updateConfig(key, value) {
   if (key === "WEATHER_API_KEY") {
     const isValid = await validateApiKey(value);
@@ -50,9 +73,15 @@ async function updateConfig(key, value) {
       return false;
     }
   } else if (key === "BOT_TOKEN") {
-    process.env.BOT_TOKEN = value;
-    saveConfig({ BOT_TOKEN: value });
-    return true;
+    const isValid = await validateBotToken(value);
+    if (isValid) {
+      process.env.BOT_TOKEN = value;
+      saveConfig({ BOT_TOKEN: value });
+      return true;
+    } else {
+      // console.log("invalid weather api key.");
+      return false;
+    }
   } else {
     // console.log("API key not found.");
     return false;
